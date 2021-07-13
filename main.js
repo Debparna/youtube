6cba90be648a3fdebe94d12bbf4be4c4a1a92243
Deb pratiher <debpratiher@Debs-MacBook-Pro.local>
2021-07-12 22:30:31 -0700

    l

--- main.js
+++ main.js
@@ -5,8 +5,7 @@ const DISCOVERY_DOCS = [
 ];
 const SCOPES = [
     'https://www.googleapis.com/auth/yt-analytics.readonly',
-    'https://www.googleapis.com/auth/youtube.force-ssl',
-    'https://www.googleapis.com/auth/youtube'
+    'https://www.googleapis.com/auth/youtube.force-ssl'
 ];
 
 const authorizeButton = document.getElementById('authorize-button');
