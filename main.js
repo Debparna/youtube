36846f489e7885a6a4a658a5e6be4650b4b86516
Deb pratiher <debpratiher@Debs-MacBook-Pro.local>
2021-07-12 22:57:37 -0700

    l

--- main.js
+++ main.js
@@ -3,10 +3,8 @@ const CLIENT_ID = '610238259195-faicttc4up4m4d3d91n8a38kfl68r2ep.apps.googleuser
 const DISCOVERY_DOCS = [
   'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
 ];
-const SCOPES = [
-    'https://www.googleapis.com/auth/yt-analytics.readonly',
-    'https://www.googleapis.com/auth/youtube.force-ssl'
-];
+
+const SCOPES = 'https://www.googleapis.com/auth/youtube.force-ssl';
 
 const authorizeButton = document.getElementById('authorize-button');
 const signoutButton = document.getElementById('signout-button');
